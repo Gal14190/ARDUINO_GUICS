126288ca3cfcadb13e7dbfe0ed32da3eb48f3929
Gal14190 <32678384+Gal14190@users.noreply.github.com>
2017-10-10 22:46:50 +0300

    Add files via upload

---
+++ GUICS/Communication.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace GUICS
+{
+    partial class Communication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialCOM = new System.IO.Ports.SerialPort(this.components);
+            this.COM_NAME = new System.Windows.Forms.DomainUpDown();
+            this.REF_BTN = new System.Windows.Forms.Button();
+            this.RUN_BTN = new System.Windows.Forms.Button();
+            this.CLOSE_BTN = new System.Windows.Forms.Button();
+            this.status_label = new System.Windows.Forms.Label();
+            this.STATUS_PANEL = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // serialCOM
+            // 
+            this.serialCOM.BaudRate = 115200;
+            // 
+            // COM_NAME
+            // 
+            this.COM_NAME.Location = new System.Drawing.Point(26, 34);
+            this.COM_NAME.Name = "COM_NAME";
+            this.COM_NAME.Size = new System.Drawing.Size(120, 22);
+            this.COM_NAME.TabIndex = 0;
+            this.COM_NAME.Text = "COM0";
+            // 
+            // REF_BTN
+            // 
+            this.REF_BTN.Location = new System.Drawing.Point(177, 28);
+            this.REF_BTN.Name = "REF_BTN";
+            this.REF_BTN.Size = new System.Drawing.Size(75, 32);
+            this.REF_BTN.TabIndex = 1;
+            this.REF_BTN.Text = "רענון";
+            this.REF_BTN.UseVisualStyleBackColor = true;
+            this.REF_BTN.Click += new System.EventHandler(this.REF_BTN_Click);
+            // 
+            // RUN_BTN
+            // 
+            this.RUN_BTN.Location = new System.Drawing.Point(38, 82);
+            this.RUN_BTN.Name = "RUN_BTN";
+            this.RUN_BTN.Size = new System.Drawing.Size(62, 32);
+            this.RUN_BTN.TabIndex = 2;
+            this.RUN_BTN.Text = "RUN";
+            this.RUN_BTN.UseVisualStyleBackColor = true;
+            this.RUN_BTN.Click += new System.EventHandler(this.RUN_BTN_Click);
+            // 
+            // CLOSE_BTN
+            // 
+            this.CLOSE_BTN.Location = new System.Drawing.Point(146, 82);
+            this.CLOSE_BTN.Name = "CLOSE_BTN";
+            this.CLOSE_BTN.Size = new System.Drawing.Size(62, 32);
+            this.CLOSE_BTN.TabIndex = 3;
+            this.CLOSE_BTN.Text = "CLOSE";
+            this.CLOSE_BTN.UseVisualStyleBackColor = true;
+            this.CLOSE_BTN.Click += new System.EventHandler(this.CLOSE_BTN_Click);
+            // 
+            // status_label
+            // 
+            this.status_label.AutoSize = true;
+            this.status_label.Location = new System.Drawing.Point(237, 90);
+            this.status_label.Name = "status_label";
+            this.status_label.Size = new System.Drawing.Size(63, 17);
+            this.status_label.TabIndex = 4;
+            this.status_label.Text = "STATUS";
+            // 
+            // STATUS_PANEL
+            // 
+            this.STATUS_PANEL.BackColor = System.Drawing.Color.Red;
+            this.STATUS_PANEL.Location = new System.Drawing.Point(306, 90);
+            this.STATUS_PANEL.Name = "STATUS_PANEL";
+            this.STATUS_PANEL.Size = new System.Drawing.Size(18, 19);
+            this.STATUS_PANEL.TabIndex = 5;
+            // 
+            // Communication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 143);
+            this.Controls.Add(this.STATUS_PANEL);
+            this.Controls.Add(this.status_label);
+            this.Controls.Add(this.CLOSE_BTN);
+            this.Controls.Add(this.RUN_BTN);
+            this.Controls.Add(this.REF_BTN);
+            this.Controls.Add(this.COM_NAME);
+            this.Name = "Communication";
+            this.Text = "communication";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialCOM;
+        private System.Windows.Forms.DomainUpDown COM_NAME;
+        private System.Windows.Forms.Button REF_BTN;
+        private System.Windows.Forms.Button RUN_BTN;
+        private System.Windows.Forms.Button CLOSE_BTN;
+        private System.Windows.Forms.Label status_label;
+        private System.Windows.Forms.Panel STATUS_PANEL;
+    }
+}
+
---
+++ GUICS/GUICS_LIB.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace GUICS
+{
+    enum SET { INPUT, OUTPUT, HIGH, LOW }
+
+    class GUICS_LIB
+    {
+        SerialPort SerialCOM;
+        public GUICS_LIB(SerialPort SerialCOM_TEMP)
+        {
+            SerialCOM = SerialCOM_TEMP;
+        }
+
+        private struct TOKEN
+        {
+            public const string DIGITAL_SET = "M";
+            public const string SET_INPUT = "I";
+            public const string SET_OUTPUT = "O";
+
+            public const string DIGITAL_INPUT = "I";
+
+            public const string DIGITAL_OUTPUT = "O";
+            public const string SET_HIGH = "H";
+            public const string SET_LOW = "L";
+
+            public const string ANALOG_INPUT = "A";
+
+            public const char END_CHAR = '@';
+        }
+
+        public void DIGITAL_MODE(int DIGITAL_PIN, SET _SET)
+        {
+            char PIN = Convert.ToChar(DIGITAL_PIN);
+            if (SerialCOM.IsOpen)
+            {
+                try
+                {
+                    string IO = "";
+                    if (_SET == SET.INPUT)
+                    {
+                        IO = TOKEN.SET_INPUT;
+                    }
+                    else if (_SET == SET.OUTPUT)
+                    {
+                        IO = TOKEN.SET_OUTPUT;
+                    }
+
+                    SerialCOM.Write(TOKEN.DIGITAL_SET + IO + PIN);
+                }
+                catch
+                {
+                    MessageBox.Show("בעיה בחיבור");
+                }
+            }
+        }
+
+        public bool DIGITAL_GET(int DIGITAL_PIN)
+        {
+            char PIN = Convert.ToChar(DIGITAL_PIN);
+
+            if (SerialCOM.IsOpen)
+            {
+                try
+                {
+                    SerialCOM.Write(TOKEN.DIGITAL_INPUT + PIN);
+                }
+                catch
+                {
+                    MessageBox.Show("בעיה בחיבור");
+                }
+
+
+                if (SerialCOM.ReadChar() == '1') return true;
+                else return false;
+            }
+            return false;
+        }
+
+        public void DIGITAL_SET(int DIGITAL_PIN, SET _SET)
+        {
+            char PIN = Convert.ToChar(DIGITAL_PIN);
+
+            if (SerialCOM.IsOpen)
+            {
+                try
+                {
+                    string IO = "";
+                    if (_SET == SET.HIGH)
+                    {
+                        IO = TOKEN.SET_HIGH;
+                    }
+                    else if (_SET == SET.LOW)
+                    {
+                        IO = TOKEN.SET_LOW;
+                    }
+
+                    SerialCOM.Write(TOKEN.DIGITAL_OUTPUT + IO + PIN);
+                }
+                catch
+                {
+                    MessageBox.Show("בעיה בחיבור");
+                }
+            }
+        }
+
+        public string ANALOG_GET(int ANALOG_PIN)
+        {
+            char PIN = Convert.ToChar(ANALOG_PIN);
+            string dataReturn = "";
+
+            if (SerialCOM.IsOpen)
+            {
+                try
+                {
+                    SerialCOM.Write(TOKEN.ANALOG_INPUT + PIN);
+
+                    int dataRead = 0;
+
+                    while (dataRead != TOKEN.END_CHAR)
+                    {
+                        dataRead = SerialCOM.ReadChar();
+                        if(dataRead != TOKEN.END_CHAR)
+                        {
+                            dataReturn += Convert.ToChar(dataRead);
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("בעיה בחיבור");
+                }
+            }
+            return dataReturn;
+        }
+    }
+
+    class GUI
+    {
+        public static void delay(int ms)
+        {
+            System.Threading.Thread.Sleep(ms);
+        }
+
+        public static void text(Control ctl, string content)
+        {
+            ctl.Invoke(new Action(() => ctl.Text = content));
+        }
+
+        public static void color(Control ctl, System.Drawing.Color color)
+        {
+            ctl.Invoke(new Action(() => ctl.BackColor = color));
+        }
+    }
+
+    class MultiTask 
+    {
+        public MultiTask( Action setup, Action loop)
+        {
+            setup();
+
+            Task.Run(() => { while(true) loop(); });
+        }
+    }
+}
